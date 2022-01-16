0a95a627cc7384c87c9004ea018463e7b1e415e4
Heidi Fryzell <hfryzell@gmail.com>
2022-01-16 14:46:38 -0500

    added background video

--- app-pages.js
+++ app-pages.js
@@ -1,10 +1,27 @@
 //use an array to render the page links
-const pageLinks = ["1", "2", "2-1", "2-2", "2-3"];
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
+
+const pageLinks = ["1", "2", "2-1", "2-2", "2-3", "2-4"];
 
 const progressLinksPages = document.getElementById('progress-links-pages');
 
 for (let i = 0; i < pageLinks.length; i++) {
     progressLinksPages.innerHTML +=
         `<a href="../../days/day-${pageLinks[i]}/day-${pageLinks[i]}.html">${pageLinks[i]}</a>  `
-
 }
