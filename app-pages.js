9878ea595b09ab77b893f164c0f8422351bd230e
Heidi Fyrzell PC <hfryzell+pc@gmail.com>
2022-01-17 14:33:40 -0500

    random quote pulling in

--- app-pages.js
+++ app-pages.js
@@ -17,7 +17,7 @@ function myFunction() {
     }
 }
 
-const pageLinks = ["1", "2", "2-1", "2-2", "2-3", "2-4", "2-5"];
+const pageLinks = ["1", "2", "2-1", "2-2", "2-3", "2-4", "2-5","3"];
 
 const progressLinksPages = document.getElementById('progress-links-pages');
 
@@ -25,3 +25,34 @@ for (let i = 0; i < pageLinks.length; i++) {
     progressLinksPages.innerHTML +=
         `<a href="../../days/day-${pageLinks[i]}/day-${pageLinks[i]}.html">${pageLinks[i]}</a>  `
 }
+
+// Powered by Quotable
+// https://github.com/lukePeavey/quotable
+
+document.addEventListener("DOMContentLoaded", () => {
+    // DOM elements
+    const button = document.getElementById("quoteBtn");
+    const quote = document.querySelector("blockquote p");
+    const cite = document.querySelector("blockquote cite");
+  
+    async function updateQuote() {
+      // Fetch a random quote from the Quotable API
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      if (response.ok) {
+        // Update DOM elements
+        quote.textContent = data.content;
+        cite.textContent = data.author;
+      } else {
+        quote.textContent = "An error occured";
+        console.log(data);
+      }
+    }
+  
+    // Attach an event listener to the `button`
+    button.addEventListener("click", updateQuote);
+  
+    // call updateQuote once when page loads
+    updateQuote();
+  });
+  
